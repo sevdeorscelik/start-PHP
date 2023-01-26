625a54efac7eccb1650d3c381715bf618aefb55e
sevdeorscelik <sevdeorscelik@gmail.com>
2023-01-26 11:34:06 +0100

    incrementing-decrementing-operators

--- 7-comparison-operators.php
+++ 7-comparison-operators.php
@@ -26,7 +26,11 @@ var_dump($a); //bool(true) verir
 $a = 5;
 $b = 2;
 
+echo "<br>";
+
 var_dump(value: $a < $b); //bool(false) 
+
+echo "<br>";
 
 var_dump(value: $a <=> $b); //int(1) a>b ise 1, a<b ise -1, a=b is 0. 
 
