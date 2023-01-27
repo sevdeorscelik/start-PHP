ca036ce6fe742f4f675dbef2f3325d80e0b22845
sevdeorscelik <sevdeorscelik@gmail.com>
2023-01-27 13:39:51 +0100

    arrays function

---
+++ 15-array-functions/15-array-functions.php
@@ -0,0 +1,52 @@
+<?php
+
+// print_r(); variable ya da arrayi ekranda gösterir
+// var_dump(); bir variable'i detayli özellikleri ile gösterir
+// explode(); //bir dizenin elemanlarini ayirir.
+// implode(); //bir deizenin elemanlarini birlestirip tek dizge yapar.
+// count(); //array'in eleman sayisini verir
+// is_array(); //arrayse 1, degilse 0 verir.
+
+
+
+$categories = [ 
+    [ 
+        'id' => 1 ,
+        'name' => 'Back - end ' 
+    ], 
+    [ 
+        'id' => 2 ,
+        'name' => 'Front - End ' 
+    ], 
+    [ 
+        'id' => 3 ,
+        'name' => 'mobile' 
+    ], 
+   
+];
+
+$var = ['value1', 'value2', 'value3'];
+
+
+//print_r($categories);
+//var_dump($categories);
+
+$imp = implode('=', $var);   //value1=value2=value3
+//print_r($imp);
+
+$exp = explode('=', $imp);
+print_r($exp);
+
+echo count($var); //3
+
+echo is_array($var); //1
+
+
+
+
+
+
+
+
+
+
