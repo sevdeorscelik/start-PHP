536579df667c644115aee511a76d2274bce37796
sevdeorscelik <sevdeorscelik@gmail.com>
2023-02-22 16:34:47 +0100

    d-array-functions-4

---
+++ 15-array-functions/d-array-functions-4.php
@@ -0,0 +1,12 @@
+<?php
+
+// current();       - Gibt das aktuelle Element in einem Array zurück
+// end();           - Setzt den internen Zeiger eines Arrays auf sein letzes Element
+// next();          - Erhöht den internen Zeiger eines Arrays
+// prev();          - Ruft den internen Array-Zeiger ab.
+// reset();         - Positioniert den internen Zeiger eines Arrays auf das erste Element
+// extract();       - Variablen in einem Array in die Symboltabelle aufnehmen
+// asort();         - Sortiert die Werte eines Arrays vom kleinesten zum größten, ohne ihre Beziehung zu sein Keys zu unterbrechen.
+// arsort();        - Sortiert die werte eines arrays vom größten zum kleinsten, ohne ihre beziehung zu sehen keys zu unterbrachen
+// ksort();         - Sortiert ein Array nach seinen Keys vom kleinsten zum größten.
+// krsort();        - Sortiert ein array nach keys vom größten zum kleinsten. 
